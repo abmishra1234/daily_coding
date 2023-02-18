011f36f9b3d6252afb924826eab089075f7d8e39
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-02-19 04:04:01 +0530

    added first optimization sample

--- ImprovementInCSharp/Daily_Coding_CSharp/Program.cs
+++ ImprovementInCSharp/Daily_Coding_CSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,22 @@ namespace Daily_Coding_CSharp
         static void Main(string[] args)
         {
             // Do the Practice for CSharp Coding here
+            const int size = 10000;
+            List<int> li = new List<int>();
+            Stopwatch sw = new Stopwatch();
+            
+            sw.Start();
+            for(int i=0; i<size; i++)  li.Add(i);
+            sw.Stop();
 
+            Console.WriteLine(" without capacity init" + sw.ElapsedTicks);
 
+            List<int> newli = new List<int>(size+5);
+            sw.Restart();
+            for (int i = 0; i < size; i++) newli.Add(i);
+            sw.Stop();
+
+            Console.WriteLine("with capacity init" + sw.ElapsedTicks);
 
 
 
